bfd2b32b6d12568b7198869a648fd799796ca0f8
dennnis0204 <dennnis0204@gmail.com>
2021-08-25 19:26:15 +0200

    Add: User model

---
+++ src/main/java/com/desiredchargingstation/map/model/AuthProvider.java
@@ -0,0 +1,6 @@
+package com.desiredchargingstation.map.model;
+
+public enum AuthProvider {
+    google,
+    facebook
+}
---
+++ src/main/java/com/desiredchargingstation/map/model/User.java
@@ -0,0 +1,31 @@
+package com.desiredchargingstation.map.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+
+@Data
+@Entity
+@Table(name = "users", uniqueConstraints = {@UniqueConstraint(columnNames = "email")})
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "users_id_seq")
+    @SequenceGenerator(name = "users_id_seq", sequenceName = "users_id_seq", allocationSize = 1)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Email
+    @Column(nullable = false)
+    private String email;
+
+    private String password;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private AuthProvider provider;
+}
