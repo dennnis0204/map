682b66222140dfb803585d1c507b234446977e97
dennnis0204 <dennnis0204@gmail.com>
2021-12-05 17:36:13 +0100

    Change: add .yml to .ignore

--- src/main/java/com/desiredchargingstation/map/config/SecurityConfig.java
+++ src/main/java/com/desiredchargingstation/map/config/SecurityConfig.java
@@ -5,26 +5,39 @@ import com.desiredchargingstation.map.security.RestAuthenticationEntryPoint;
 import com.desiredchargingstation.map.security.oauth2.CustomOAuth2UserService;
 import com.desiredchargingstation.map.security.oauth2.CustomOidcUserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
 import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
-@Profile({"dev", "test"})
-public class SecurityConfigDev extends WebSecurityConfigurerAdapter {
+@Profile({"prod"})
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Value("${settings.host}")
+    private String host;
 
     private final CustomUserDetailsService customUserDetailsService;
     private final CustomOAuth2UserService customOAuth2UserService;
@@ -35,12 +48,36 @@ public class SecurityConfigDev extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
+    @Component
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    public class ConfigCtrl implements Filter {
+        @Override
+        public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+            final HttpServletResponse response = (HttpServletResponse) res;
+            response.setHeader("Access-Control-Allow-Origin", "*");
+            response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
+            response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type");
+            response.setHeader("Access-Control-Max-Age", "3600");
+            if ("OPTIONS".equalsIgnoreCase(((HttpServletRequest) req).getMethod())) {
+                response.setStatus(HttpServletResponse.SC_OK);
+            } else {
+                chain.doFilter(req, res);
+            }
+        }
+        @Override
+        public void destroy() {
+        }
+        @Override
+        public void init(FilterConfig config) throws ServletException {
+        }
+    }
+
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedOrigins(Arrays.asList(host));
         configuration.setAllowCredentials(true);
-        configuration.setAllowedMethods(Arrays.asList("GET","POST", "DELETE", "PUT"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST", "DELETE", "PUT", "OPTIONS"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
@@ -73,7 +110,7 @@ public class SecurityConfigDev extends WebSecurityConfigurerAdapter {
                 .and()
 
                 .authorizeRequests()
-                .antMatchers("/", "/favicon.ico", "/login", "/all-points")
+                .antMatchers("/", "/favicon.ico", "/login", "/all-points","/static/**", "/js/**", "/css/**")
                 .permitAll()
 
                 .anyRequest()
@@ -86,13 +123,13 @@ public class SecurityConfigDev extends WebSecurityConfigurerAdapter {
                         .userInfoEndpoint(userInfo -> userInfo
                                 .userService(customOAuth2UserService)
                                 .oidcUserService(customOidcUserService))
-                        .defaultSuccessUrl("http://localhost:3000", true)
+                        .defaultSuccessUrl(host, true)
                         .failureUrl("/")
                 )
 
                 .logout(logout -> logout
                         .deleteCookies("JSESSIONID")
-                        .logoutSuccessUrl("http://localhost:3000")
+                        .logoutSuccessUrl(host)
                 );
 
     }
