9f9f6c4e7a94afa9da9e59b17a3af0dd91fb78c0
dennnis0204 <dennnis0204@gmail.com>
2021-12-12 08:03:52 +0100

    Add: csrf token configuration added

--- src/main/java/com/desiredchargingstation/map/config/SecurityConfig.java
+++ src/main/java/com/desiredchargingstation/map/config/SecurityConfig.java
@@ -15,6 +15,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 @EnableWebSecurity
@@ -44,6 +45,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .csrf(csrf -> csrf
+                        .csrfTokenRepository((CookieCsrfTokenRepository.withHttpOnlyFalse())))
 
                 .exceptionHandling()
                 .authenticationEntryPoint(new RestAuthenticationEntryPoint())
